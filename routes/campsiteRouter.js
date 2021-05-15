522241bb5cc57c02d0bb0c392bdeafdb153de887
Sean Browne <sbrowne15@gmail.com>
2021-05-15 12:33:41 -0500

    Week 1 Workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,25 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.write(`Updatind campsite: ${req.params.campsiteId} \n`);
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /campsites/${req.params.campsiteId}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
 module.exports = campsiteRouter;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-const campsiteRouter = require('./routes/campsiteRouter');
 
+
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -10,7 +13,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
-
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
